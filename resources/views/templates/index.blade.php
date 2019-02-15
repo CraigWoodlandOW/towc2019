65fe42d134dba1e926c973af5eadddaadbf0a0a1
jamie <jamie@outstandingweb.co.uk>
2019-02-15 13:19:52 +0000

    mods

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -9,7 +9,7 @@
         <img src="/images/large-logo.svg" alt="Outstanding web large logo"/>
     </div>
 </div>
-<div class="row no-gutters">
+<div class="row no-gutters mb-5">
     <div class="col-md-6 offset-md-3 text-center">
         @absence([ 'row1-section2', '<h1><strong>OUTSTANDING</strong>….it’s who we are!</h1>
         <p>We are an award-winning web design agency based in the heart of the Bedfordshire countryside.
@@ -19,14 +19,14 @@
         @render([ 'row1-section2' ])
     </div>
 </div>
-<div class="row no-gutters blur-row">
+<div class="row no-gutters blur-row my-5">
     <div class="col-md-12 blur-image-holder">
         <img src="/images/large-lizard.jpg" alt="what we do"/>
     </div>
     <div class="col-md-7 copy-box">
     
     </div>
-    <div class="col-md-5 blur-box">
+    <div class="col-md-5 blur-box lizard-blur">
         <div class="row no-gutters h-100">
             <div class="col-md-12 align-self-center justify-content-center text-center">
                 <h2>What We do</h2>
@@ -34,5 +34,88 @@
             </div>
         </div>
     </div>
+</div>
+<div class="row no-gutters blur-row my-5">
+    <div class="col-md-12 blur-image-holder">
+        <img src="/images/large-parrot.jpg" alt="what we do"/>
+    </div>
+    <div class="col-md-5 blur-box parrot-blur">
+        <div class="row no-gutters h-100">
+            <div class="col-md-12 align-self-center justify-content-center text-center">
+                <h2>Our Work</h2>
+                <a href="#" class="read-more text-center">Read More</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-7 copy-box">
+    
+    </div>
+    
+</div>
+<div class="row no-gutters blur-row my-5">
+    <div class="col-md-12 blur-image-holder">
+        <img src="/images/large-leopord.jpg" alt="what we do"/>
+    </div>
+    <div class="col-md-7 copy-box">
+    
+    </div>
+    <div class="col-md-5 blur-box leopord-blur">
+        <div class="row no-gutters h-100">
+            <div class="col-md-12 align-self-center justify-content-center text-center">
+                <h2>Meet the team</h2>
+                <a href="#" class="read-more text-center">Read More</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row no-gutters blur-row my-5">
+    <div class="col-md-12 blur-image-holder">
+        <img src="/images/large-zebra.jpg" alt="what we do"/>
+    </div>
+    <div class="col-md-5 blur-box zebra-blur">
+        <div class="row no-gutters h-100">
+            <div class="col-md-12 align-self-center justify-content-center text-center">
+                <h2>Knowledge</h2>
+                <a href="#" class="read-more text-center">Read More</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-7 copy-box">
+    
+    </div>
+    
+</div>
+<div class="row no-gutters blur-row my-5">
+    <div class="col-md-12 blur-image-holder">
+        <img src="/images/large-custom-software.jpg" alt="what we do"/>
+    </div>
+    <div class="col-md-7 copy-box">
+    
+    </div>
+    <div class="col-md-5 blur-box fish-blur">
+        <div class="row no-gutters h-100">
+            <div class="col-md-12 align-self-center justify-content-center text-center">
+                <h2>Blog</h2>
+                <a href="#" class="read-more text-center">Read More</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row no-gutters blur-row my-5">
+    <div class="col-md-12 blur-image-holder">
+        <img src="/images/large-penguin.jpg" alt="what we do"/>
+    </div>
+    <div class="col-md-5 blur-box penguin-blur">
+        <div class="row no-gutters h-100">
+            <div class="col-md-12 align-self-center justify-content-center text-center">
+                <h2>Contact Us</h2>
+                <a href="#" class="read-more text-center">Read More</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-7 copy-box">
+    
+    </div>
+    
 </div>
 @endsection
